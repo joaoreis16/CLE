03dfef1f76b1ec2c9d1892f6c404e3f73bea7d81
reis <joaoreis16@ua.pt>
2023-03-06 16:04:29 +0000

    ex2 - done

--- P2/sortInt.c
+++ P2/sortInt.c
@@ -9,15 +9,58 @@
 //                                                                     //
 /////////////////////////////////////////////////////////////////////////
 
+void print(int *val, int N) {
+    for (int j = 0; j < N; j++) {
+        printf("%d\n", val[j]);
+    } 
+}
+
 void validate(int *val, int N) {
     int i;
-    for (i = 0; i < N - 1; i++) {
+    for (i = 0; i < N; i++) {
+        
+        /* printf("i   = %d\n", i);
+        printf("N-1 = %d\n", N - 1); */
+
+        if (i == (N - 1))  {
+            printf ("Everything is OK!\n");
+            break;
+        }
+
         if (val[i] > val[i+1]) { 
             printf ("Error in position %d between element %d and %d\n", i, val[i], val[i+1]);
             break;
         }
 
-        if (i == (N - 1)) printf ("Everything is OK!\n");
+    }
+}
+
+void quicksort(int *arr, int left, int right) {
+    if (left < right) {
+        int pivot = arr[(left + right) / 2];
+        int i = left - 1;
+        int j = right + 1;
+
+        while (1) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i >= j) {
+                break;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        quicksort(arr, left, j);
+        quicksort(arr, j+1, right);
     }
 }
 
@@ -52,7 +95,10 @@ int main(int argc, char *argv[]) {
     }
     printf("number of values = %d\n", N_values);
 
-    int sequence[N_values];
+    // int sequence[N_values];
+    int *sequence;
+    sequence = (int*)malloc(N_values * sizeof(int));
+
     int i = 0;
 
     // Read the contents of the file
@@ -64,11 +110,12 @@ int main(int argc, char *argv[]) {
     
     // Close the file
     fclose(file);
+
+    quicksort(sequence, 0, N_values - 1);
     
     // Print the integers read from the file
-    for (int j = 0; j < N_values; j++) {
-        printf("%d\n", sequence[j]);
-    }
+    // printf("sorted sequence:\n");
+    // print(sequence, N_values);
 
     validate(sequence, N_values);
     return 0;
